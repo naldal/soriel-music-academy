2588e32af79982b1605c012f2a609f743bbe4249
naldal <cec7378@gmail.com>
2020-07-29 18:38:26 +0900

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,13 +16,13 @@ if [ -z "$CURRENT_PID" ]; then
         echo ">현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
         echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
+        kill -9 $CURRENT_PID
         sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/|*.jar|tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR NAME : $JAR_NAME"
 
