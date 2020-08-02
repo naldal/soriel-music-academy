ca3ba122d2fe11fcfc21e1394975b2028fcea75e
naldal <cec7378@gmail.com>
2020-08-02 19:52:37 +0900

    load students data to order by asc

--- src/main/java/com/soriel/music/springboot/domain/student/StudentsRepository.java
+++ src/main/java/com/soriel/music/springboot/domain/student/StudentsRepository.java
@@ -1,8 +1,12 @@
 package com.soriel.music.springboot.domain.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentsRepository extends JpaRepository<Students, Long> {
+    @Query(value = "select * from students order by student_name asc", nativeQuery = true)
+    List<Students> findAll();
 }
